d69c2c52987ded17df74fd22f09b53d1b1748b24
melih <b2200356062@cs.hacettepe.edu.tr>
2023-10-08 19:42:37 +0300

    added documentation

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+// three js lib
 import * as THREE from 'three'
+// kamera kontrolleri
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// sliderlar için gui
 import {GUI} from 'dat.gui'
 
 const WIDTH = window.innerWidth;
@@ -8,15 +11,15 @@ const HEIGHT = window.innerHeight;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 70, WIDTH / HEIGHT, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer({powerPreference: "high-performance"}); 
-// antialias: - 40 fps
+// antialias: - 40 fps, kötü performans
 
 camera.position.z = 50; // uzaklık
 
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor(0x444444, 1);
+renderer.setClearColor(0x444444, 1); // arkaplan rengi
 document.body.appendChild( renderer.domElement );
 
-
+// küp
 const geometry = new THREE.BoxGeometry( 10, 10, 10 );
 
 // what covers an object; color, texture etc.
@@ -30,11 +33,13 @@ scene.add(cube);
 
 cube.position.x = 0;
 
+// torus?? objesi
 const torusGeometry = new THREE.TorusGeometry(7,1,6,12);
 const phongMaterial = new THREE.MeshPhongMaterial({color: 0xdddddd});
 const torus = new THREE.Mesh(torusGeometry, phongMaterial);
 //scene.add(torus);
 
+// küre
 const sphere = new THREE.SphereGeometry(7, 30, 30);
 const sphereMaterial = new THREE.MeshLambertMaterial({color: 0x555555});
 const sphereMesh = new THREE.Mesh(sphere, sphereMaterial);
@@ -42,20 +47,23 @@ sphereMesh.position.x = 0;
 sphereMesh.position.y = 20;
 //scene.add(sphereMesh);
 
+// yer düzlemi
 const planeGeometry = new THREE.PlaneGeometry(100, 100, 100);
 const planeMaterial = new THREE.MeshLambertMaterial({color: 0x555555});
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+// rotation olmazsa düzlem yerde olmuyor
 plane.rotation.x = -0.5 * Math.PI; 
 plane.position.y = -10;
-
 scene.add(plane);
 
+// dodecahedron
 const dodecahedronGeometry = new THREE.DodecahedronGeometry(7);
 const lambertMaterial = new THREE.MeshLambertMaterial({ color: 0xdddddd});
 const dodecahedron = new THREE.Mesh(dodecahedronGeometry, lambertMaterial);
 dodecahedron.position.x = 25;
 scene.add(dodecahedron);
 
+// directional light
 // const light = new THREE.DirectionalLight(0xffffff, 1);
 // light.position.set(1,0,0);
 // scene.add(light);
@@ -67,6 +75,7 @@ scene.add(pl);
 
 // mouse controls
 const controls = new OrbitControls(camera, renderer.domElement);
+
 const gui = new GUI();
 
 gui.add(pl.position, 'x', -100, 100);
@@ -74,6 +83,7 @@ gui.add(pl.position, 'y', -100, 100);
 gui.add(pl.position, 'z', -100, 100);
 
 
+// fps counter
 var fps = document.getElementById("fps");
 var startTime = Date.now();
 var frame = 0;
@@ -81,8 +91,6 @@ var frame = 0;
 function tick() {
   window.requestAnimationFrame(tick);
 }
-
-
 
 let t = 0;
 function render() {
@@ -119,7 +127,7 @@ window.addEventListener('keydown', (event) =>{
         case 'KeyA':
             camera.position.x -= 5;
             //cube.position.x -= 5;
-            
+        
             break;
         case 'KeyD':
             camera.position.x += 5;
@@ -142,7 +150,9 @@ window.addEventListener('keydown', (event) =>{
             camera.rotation.y -= Math.PI / 60;
             break;
     }
-})
+});
+
 render();
+
 
 
